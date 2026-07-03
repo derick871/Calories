9ceeab73c5b6fac7a24d272b8b645de70a1689fd
derick871 <wanyamaderrick4@gmail.com>
2026-07-03 21:49:18 +0300

    update models and views file

--- Calorie/models.py
+++ Calorie/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class client (models.model):
+    email = models.EmailField(max_length=20)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"self.email"
+    
+class subscriber (models.model):
+    email = models.EmailField(Unique=True)
+
+    def __str__(self):
+        return f"self.email"
+
--- Calorie/views.py
+++ Calorie/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def subscriber(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        if subscriber.object.filter(email=email).exists():
+            messages.error(request, 'Email already registered.')
+        else:
+            subscriber.objects.create(email=email)
+            messages.success(request, 'Successfully registered.')
+    return render(request, 'register.html')
