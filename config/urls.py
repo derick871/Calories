ade5b44843b3760f583fba6bac09bbaee578453d
derick871 <wanyamaderrick4@gmail.com>
2026-07-13 11:54:36 +0300

    update register and urls files

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from Calorie import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Calorie.urls'))
+     path('register/', views.register, name='register'),
+    path('', views.register, name='home'),
+    path('', include('Calorie.urls')),
 ]
